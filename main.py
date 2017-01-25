34549adcf8de8eeef477a79d7cb18675e83437e2
e-inquirer <sorour.a@gmail.com>
2017-01-24 20:03:42 -0500

    added random movie cycle to main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import webapp2
+import random
 
 class Index(webapp2.RequestHandler):
 
     def getRandomMovie(self):
 
         # TODO: make a list with at least 5 movie titles
+        movieList = ["x-men1", "x-men2", "x-men3", "rocky1", "rocky2", "rocky3"]
 
         # TODO: randomly choose one of the movies, and return it
+        index = random.randint(0, 5)
 
-        return "The Big Lebowski"
+        return movieList[index]
 
     def get(self):
         # choose a movie by invoking our new function
@@ -19,7 +22,11 @@ class Index(webapp2.RequestHandler):
         content += "<p>" + movie + "</p>"
 
         # TODO: pick a different random movie, and display it under
-        # the heading "<h1>Tommorrow's Movie</h1>"
+        # the heading "<h1>Tomorrow's Movie</h1>"
+        tomorrow_movie = self.getRandomMovie()
+
+        content += "<h1>Tomorrow's Movie</h1>"
+        content += "<p>" + tomorrow_movie + "</p>"
 
         self.response.write(content)
 
